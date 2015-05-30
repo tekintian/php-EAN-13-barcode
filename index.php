0e93c3bc85ac5b62d0d39ac0eee436fb6a7d09ab
tekin <tekin@yunnan.ws>
2015-05-30 12:09:33 +0800

    add barcode
    PHP 电子商品条形码生成系统， 可生成13位的数字条形码， 输入12位数字就可以，最后一位（第13位）是效验码，自动生成的哦。

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+define('BARCODE', true);
+//autoload
+include("barcode_lib.php");
+
+class Barcode
+{
+   public $number;
+   public $encoding;
+   public $scale;
+
+   protected $_encoder;
+
+   function __construct($encoding, $number=null, $scale=null)
+   {
+      $this->number = ($number==null) ? $this->_random() : $number;
+      $this->scale = ($scale==null || $scale<4) ? 4 : $scale;
+
+      // Reflection Class : Method
+
+      $this->_encoder = new EAN13($this->number, $this->scale);
+   }
+
+   function __destruct()
+   {
+      $this->_encoder->display();
+   }
+
+   private function _random()
+   {
+     return substr(number_format(time() * rand(),0,'',''),0,12);
+   }
+}
+
+$encoding = (isset($_GET['encoding'])) ? $_GET['encoding'] : 'EAN-13';
+$number   = (isset($_GET['code']))     ? $_GET['code']     : null;
+$scale    = (isset($_GET['scale']))    ? $_GET['scale']    : null;
+
+new Barcode($encoding, $number, $scale);
